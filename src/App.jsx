d7a3b1bf667f88360bb2555371a7b6cefecc58b4
bharat <bharatdhiman40@gmail.com>
2024-08-16 15:51:39 +0530

    separated login and sign in page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,19 @@
+import { useState } from "react";
 import farmBG2 from "./assets/images/farmBG2.jpg";
 import LoginPage from "./components/LoginPage";
+import SignIn from "./components/SignIn";
 const App = () => {
+  const [comp, setComp] = useState(<LoginPage />);
+  const [data, setData] = useState("New User?");
+  const changeData = () => {
+    if (data === "New User?" || comp == <LoginPage />) {
+      setData("Already a User?");
+      setComp(<SignIn />);
+    } else {
+      setData("New User?");
+      setComp(<LoginPage />);
+    }
+  };
   return (
     <div className="flex">
       <img
@@ -9,7 +22,13 @@ const App = () => {
         className="hidden lg:block w-1/2 h-screen"
       />
       <div className="flex flex-col justify-center items-center text-center h-screen lg:w-1/2 max-lg:w-full bg-gray-200 font-mono">
-        <LoginPage />
+        {comp}
+        <button
+          onClick={changeData}
+          className="px-3 py-2 rounded-md bg-emerald-500 text-white font-bold my-2 hover:bg-emerald-600 shadow-md duration-500"
+        >
+          {data}
+        </button>
       </div>
     </div>
   );
--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -1,7 +1,7 @@
-const LoginPage = () => {
+const SignIn = () => {
   return (
     <div className=" flex flex-col gap-8 items-center justify-center max-h-max max-w-max border  shadow-md p-8 rounded-md bg-gray-100">
-      <h1 className="text-3xl font-bold text-emerald-500">WELCOME BACK!</h1>
+      <h1 className="text-3xl font-bold text-emerald-500">NEW ACCOUNT?</h1>
       <div className="flex flex-col gap-3 text-center">
         <div className="flex gap-3">
           <span>👤</span>
@@ -12,6 +12,22 @@ const LoginPage = () => {
           />
         </div>
         <div className="flex gap-3">
+          <span>📞</span>
+          <input
+            type="tel"
+            placeholder="Phone Number"
+            className="p-1 border shadow-sm  bg-gray-100 outline-green-500"
+          />
+        </div>
+        <div className="flex  gap-3">
+          <span>✉️</span>
+          <input
+            type="email"
+            placeholder="Mail"
+            className="p-1 border shadow-sm  bg-gray-100 outline-green-500"
+          />
+        </div>
+        <div className="flex  gap-3">
           <span>🔑</span>
           <input
             type="password"
@@ -19,19 +35,24 @@ const LoginPage = () => {
             className="p-1 border shadow-sm t bg-gray-100 outline-green-500"
           />
         </div>
-        <div className="flex flex-col my-4 gap-3">
+        <div className="flex gap-3">
+          <span>🔑</span>
+          <input
+            type="password"
+            placeholder="Confirm Password"
+            className="p-1 border shadow-sm  bg-gray-100 outline-green-500"
+          />
+        </div>
+        <div className="flex flex-col gap-4 my-5">
           <button
             type="submit"
             className="py-2 px-1 font-semibold bg-emerald-500 hover:bg-emerald-600 text-white shadow-md rounded-md duration-500"
           >
-            login
+            Register
           </button>
-          <span className="text-right hover:text-emerald-500 hover:underline">
-            forgot password?
-          </span>
         </div>
       </div>
     </div>
   );
 };
-export default LoginPage;
+export default SignIn;
